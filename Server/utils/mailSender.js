ebde6e856e312c1fb374d1c4180145f507778743
Aman Pandey <amanpandey7651@gmail.com>
2026-07-23 22:20:02 +0530

    Fix mailSender ReferenceError

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -1,26 +1,54 @@
-const nodemailer = require('nodemailer');
-const mailSender = async(email,title,body)=>{
-    try{
-        let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            auth:{
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
-            }
-        })
-        let info = await transporter.sendMail({
-            from:'StudyNotion || Codehelp - By Aman',
-            to:email,
-            subject: title,
-            html:body,
-        })
-        console.log(info);
-        return info;
-    }
-    catch(err){
-        console.log(info);
-        return info;
+// const nodemailer = require('nodemailer');
+// const mailSender = async(email,title,body)=>{
+//     try{
+//         let transporter = nodemailer.createTransport({
+//             host: process.env.MAIL_HOST,
+//             auth:{
+//                 user: process.env.MAIL_USER,
+//                 pass: process.env.MAIL_PASS,
+//             }
+//         })
+//         let info = await transporter.sendMail({
+//             from:'StudyNotion || Codehelp - By Aman',
+//             to:email,
+//             subject: title,
+//             html:body,
+//         })
+//         console.log(info);
+//         return info;
+//     }
+//     catch(err){
+//         console.log(info);
+//         return info;
 
-        }
-}
+//         }
+// }
+// module.exports = mailSender;
+const nodemailer = require("nodemailer");
+
+const mailSender = async (email, title, body) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: "StudyNotion || CodeHelp - By Aman",
+      to: email,
+      subject: title,
+      html: body,
+    });
+
+    console.log("Email sent successfully:", info.messageId);
+    return info;
+  } catch (err) {
+    console.error("Error occurred while sending mail:", err);
+    throw err;
+  }
+};
+
 module.exports = mailSender;
